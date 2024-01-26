36a7e4951eeb4bacd4abcbd9a5ef32ec9333f33b
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2024-01-27 01:15:39 +0200

    validate login form

--- client/src/Pages/auth/Login/Login.js
+++ client/src/Pages/auth/Login/Login.js
@@ -1,17 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AuthBox from "../../../components/authBox";
 import LoginHeader from './LoginHeader';
 import LoginPageInput from './LoginPageInput';
 import LoginPageFooter from './LoginPageFooter';
+import { validateLoginForm } from '../../../utils/validator';
 
 const Login = () => {
   const [mail, setMail] = useState('');
   const [password, setPassword] = useState('');
 
   const [isFormValid, setIsFormValid] = useState(false);
+  useEffect(() => {
+    setIsFormValid(validateLoginForm({ mail, password }))
+  }, [mail, password, setIsFormValid]);
 
   const handleLogin = () => {
-
+    console.log(mail)
+    console.log(password)
   }
   return (
     <AuthBox>
--- client/src/Pages/auth/Login/LoginPageFooter.js
+++ client/src/Pages/auth/Login/LoginPageFooter.js
@@ -3,24 +3,40 @@ import { useNavigate } from 'react-router-dom';
 
 import CustomPrimaryButton from '../../../components/CustomPrimaryButton'
 import RedirectInfo from '../../../components/RedirectInfo'
+import { Tooltip } from '@mui/material';
 
 const LoginPageFooter = ({ handleLogin, isFormValid }) => {
 
     const navigate = useNavigate();
+
     const handelPushToRegisterPage = () => {
         navigate('/register')
-    }
+    };
+
+    const getFormNotValidMessage = () => {
+        return "Enter Correct Email Address and Password should contain between 6  and 12 characters"
+    };
+
+    const getFormValidMessage = () => {
+        return "press to login...!"
+    };
 
     return (
         <>
-            <div>
-                <CustomPrimaryButton
-                    label="Login"
-                    additionalStyles={{ marginTop: "30px" }}
-                    disabled={isFormValid}
-                    onClick={handleLogin}
-                />
-            </div>
+            <Tooltip
+                title={!isFormValid ? getFormNotValidMessage() : getFormValidMessage()}
+            >
+
+                <div>
+                    <CustomPrimaryButton
+                        label="Login"
+                        additionalStyles={{ marginTop: "30px" }}
+                        disabled={!isFormValid}
+                        onClick={handleLogin}
+                    />
+                </div>
+            </Tooltip>
+
             <RedirectInfo
                 text="need an Account? "
                 redirectText="create account"
