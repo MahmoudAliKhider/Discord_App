49b0d526b7b53d5bf094c60a3c0ded80069559b3
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2024-01-27 00:55:07 +0200

    footer login page

--- client/src/Pages/auth/Login/LoginPageFooter.js
+++ client/src/Pages/auth/Login/LoginPageFooter.js
@@ -1,14 +1,33 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
 import CustomPrimaryButton from '../../../components/CustomPrimaryButton'
+import RedirectInfo from '../../../components/RedirectInfo'
 
 const LoginPageFooter = ({ handleLogin, isFormValid }) => {
+
+    const navigate = useNavigate();
+    const handelPushToRegisterPage = () => {
+        navigate('/register')
+    }
+
     return (
-        <CustomPrimaryButton
-            label="Login"
-            additionalStyles={{ marginTop: "30px" }}
-            disabled={isFormValid}
-            onClick={handleLogin}
-        />
+        <>
+            <div>
+                <CustomPrimaryButton
+                    label="Login"
+                    additionalStyles={{ marginTop: "30px" }}
+                    disabled={isFormValid}
+                    onClick={handleLogin}
+                />
+            </div>
+            <RedirectInfo
+                text="need an Account? "
+                redirectText="create account"
+                additionalStyles={{ marginTop: "5px" }}
+                redirectHandler={handelPushToRegisterPage}
+            />
+        </>
     )
 }
 
---
+++ client/src/components/RedirectInfo.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Typography, styled } from '@mui/material'
+
+const RedirectText = styled("span")({
+    color: "#00AFF4",
+    fontWeight: 500,
+    cursor: "pointer"
+})
+
+const RedirectInfo = ({
+    text,
+    redirectText,
+    additionalStyles,
+    redirectHandler,
+}) => {
+    return (
+        <Typography
+            sx={{ color: "#72767d" }}
+            style={additionalStyles ? additionalStyles : {}}
+            variant='subtitle2'
+        >
+            {text}
+            <RedirectText onClick={redirectHandler}>{redirectText}</RedirectText>
+        </Typography>
+    )
+}
+
+export default RedirectInfo
