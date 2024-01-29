21756221032d771ab3bd5a26d28031ef74bad66c
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2024-01-30 01:34:24 +0200

    dropdown menu

--- client/src/Pages/Dashbord/AppBar/AppBar.js
+++ client/src/Pages/Dashbord/AppBar/AppBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { styled } from '@mui/system';
-
+import DropdownMenu from "./DropdownMenu";
 
 const MainContainer = styled('div')({
     position: 'absolute',
@@ -18,7 +18,9 @@ const MainContainer = styled('div')({
 })
 const AppBar = () => {
     return (
-        <MainContainer></MainContainer>
+        <MainContainer>
+            <DropdownMenu />
+        </MainContainer>
     )
 }
 
---
+++ client/src/Pages/Dashbord/AppBar/DropdownMenu.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { IconButton } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Logout } from "./../../../utils/auth";
+
+export default function BasicMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton onClick={handleMenuOpen} style={{ color: "white" }}>
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleMenuClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={Logout}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+}
