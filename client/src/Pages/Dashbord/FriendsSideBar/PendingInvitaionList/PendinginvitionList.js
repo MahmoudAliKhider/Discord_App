7ccd1ee9f3dc30309ee3ce53f72b01770eb1d44f
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2024-01-30 01:21:29 +0200

    invitation

--- client/src/Pages/Dashbord/FriendsSideBar/PendingInvitaionList/PendinginvitionList.js
+++ client/src/Pages/Dashbord/FriendsSideBar/PendingInvitaionList/PendinginvitionList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { styled } from "@mui/system";
+import PendingInvitationsListItem from "./PendingInvitationsListItem";
 
 const MainContainer = styled('div')({
   height: '22%',
@@ -10,9 +11,35 @@ const MainContainer = styled('div')({
   overflow: "auto",
 })
 
+const DUMMY_INVITATIONS = [
+  {
+    _id: "1",
+    senderId: {
+      username: "Mark",
+      mail: "dummy@ad.com",
+    },
+  },
+  {
+    _id: "2",
+    senderId: {
+      username: "John",
+      mail: "John@ad.com",
+    },
+  },
+];
+
 const PendinginvitionList = () => {
   return (
-    <MainContainer></MainContainer>
+    <MainContainer>
+      {DUMMY_INVITATIONS.map((invitation) => (
+        <PendingInvitationsListItem
+          key={invitation._id}
+          id={invitation._id}
+          username={invitation.senderId.username}
+          mail={invitation.senderId.mail}
+        />
+      ))}
+    </MainContainer>
   )
 }
 
