58189c45426b5aa28b677cad8986e4a7151083fe
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2024-02-02 22:47:53 +0200

    Choose Active Conversation

--- client/src/Pages/Dashbord/FriendsSideBar/FriendsList/FriendsListItem.js
+++ client/src/Pages/Dashbord/FriendsSideBar/FriendsList/FriendsListItem.js
@@ -3,10 +3,20 @@ import Button from "@mui/material/Button";
 import { Typography } from '@mui/material'
 import Avatar from '../../../../components/Avatar';
 import OnlineIndecator from './OnlineIndecator';
+import { chatTypes, getActions } from '../../../../Redux/actions/chatAction';
+import { useDispatch } from 'react-redux';
 
 const FriendsListItem = ({ id, username, isOnline }) => {
+
+    const dispatch = useDispatch();
+    const action = getActions(dispatch);
+
+    const handelChooseActiveConversation = () => {
+        action.setChosenChatDetails({ id: id, name: username }, chatTypes.DIRECT);
+    }
     return (
         <Button
+            onClick={handelChooseActiveConversation}
             style={{
                 width: '100%',
                 height: "42px",
