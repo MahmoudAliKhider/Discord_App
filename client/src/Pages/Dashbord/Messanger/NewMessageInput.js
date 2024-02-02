d960f570b7f64bb193c573b3c6992dcc7c3d3005
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2024-02-03 01:03:00 +0200

    create new message input

--- client/src/Pages/Dashbord/Messanger/NewMessageInput.js
+++ client/src/Pages/Dashbord/Messanger/NewMessageInput.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useSelector } from "react-redux";
+import { styled } from '@mui/system';
+
+const MainContainer = styled("div")({
+    height: "60px",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+});
+
+const Input = styled("input")({
+    backgroundColor: "#2f3136",
+    height: "98%",
+    color: "white",
+    border: "none",
+    borderRadius: "9px",
+    fontSize: "14px",
+    padding: "0 10px",
+});
 
 const NewMessageInput = () => {
-  return (
-    <div></div>
-  )
+
+    const chosenChatDetails = useSelector((state) => state.chat.chosenChatDetails);
+    const [message, setMessage] = useState("");
+
+    const handleMessageValueChange = (event) => {
+        setMessage(event.target.value);
+    };
+
+    const handelKeyPressed = (event) => {
+        if (event.key === "Enter") {
+            handelSendMessage();
+        }
+    };
+
+    const handelSendMessage = () => {
+        console.log("Sending message to the Server...");
+        setMessage("")
+    }
+
+    return (
+        <MainContainer>
+            <Input
+                placeholder={`Write Message to ${chosenChatDetails?.name}`}
+                value={message}
+                onChange={handleMessageValueChange}
+                onKeyDown={handelKeyPressed}
+            />
+        </MainContainer>
+    )
 }
 
 export default NewMessageInput
