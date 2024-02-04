43c7c3e9a5dec8bee6b45b1542848522ac0f882b
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2024-02-04 23:40:42 +0200

    create date separator

--- client/src/Pages/Dashbord/Messanger/Messages/Messages.js
+++ client/src/Pages/Dashbord/Messanger/Messages/Messages.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { styled } from '@mui/system';
 import MessagesHeader from './MessagesHeader';
 import { Message } from './Message';
+import DateSeparator from './DateSeparator';
 
 const MainContainer = styled("div")({
     height: "calc(100% -60px)",
@@ -44,19 +45,35 @@ const Messages = () => {
                             "dd/mm/yy"
                         );
 
-                    return (<Message
-                        key={message._id}
-                        content={message.content}
-                        username={message.author.username}
-                        sameAuthor={sameAuthor}
-                        date={
-                            converDateToHumanReadable(
-                                new Date(message.date),
-                                'dd/mm/yy'
+                    return (
+                        <div key={message._id} style={{ width: " 97%" }}>
+
+                            {(!sameDay || index === 0) && (
+                                <DateSeparator
+                                    date={converDateToHumanReadable(
+                                        new Date(message.date),
+                                        "dd/mm/yy"
+                                    )}
+                                        />
                             )
-                        }
-                        sameDay={sameDay}
-                    />)
+
+                            }
+
+                            <Message
+                                key={message._id}
+                                content={message.content}
+                                username={message.author.username}
+                                sameAuthor={sameAuthor}
+                                date={
+                                    converDateToHumanReadable(
+                                        new Date(message.date),
+                                        'dd/mm/yy'
+                                    )
+                                }
+                                sameDay={sameDay}
+                            />
+                        </div>
+                    )
                 })
             }
         </MainContainer>
